f623ab8f79846d5b1cbf62c9f76dfa11a6d55f80
ssudeep606 <ssudeep606@gmail.com>
2024-06-04 11:53:56 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Resume from './Pages/Resume';
 import Projects from './Pages/Projects';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
-
 function App() {
   return (
       <>
         <Header />
         <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
       <Route path='/resume' element={<Resume/>}/>
       <Route path='/project' element={<Projects/>}/>
-      <Route path='/*' element={<Navigate to={'/'}/>}/>
     </Routes>
         <Footer />
       </>
